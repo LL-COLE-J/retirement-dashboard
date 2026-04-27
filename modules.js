e0965e0b4b93653f7536f15c013a9ec89c1fa33a
LL-COLE-J <llcolej8@gmail.com>
2026-04-27 16:25:01 -0500

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,1 +1,32 @@
+let incomes=[],expenses=[],events=[];
 
+function norm(a,f){
+return a*(f==="monthly"?12:f==="biweekly"?26:1);
+}
+
+/* TAX */
+function calcTax(income, filing){
+let brackets={
+single:[[10000,0.1],[40000,0.12],[85000,0.22]],
+married_joint:[[20000,0.1],[80000,0.12],[170000,0.22]],
+head:[[15000,0.1],[55000,0.12],[90000,0.22]]
+};
+
+let tax=0,prev=0;
+for(let [limit,rate] of brackets[filing]){
+if(income>limit){
+tax+=(limit-prev)*rate;
+prev=limit;
+}else{
+tax+=(income-prev)*rate;
+break;
+}
+}
+return tax;
+}
+
+/* EVENTS */
+function addEvent(){
+events.push({id:Date.now(),year:5,type:"expense",value:10000});
+renderEvents();
+}
