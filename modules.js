aa48a28cdca1dccca9808e0be0c838e0bc8b70ae
LL-COLE-J <llcolej8@gmail.com>
2026-04-27 17:35:16 -0500

    Update modules.js

--- modules.js
+++ modules.js
@@ -4,12 +4,22 @@ let state = {
   incomes: [],
   expenses: [],
   events: [],
-  accounts: [],
+
+  accounts: [
+    {type:"checking", balance:0},
+    {type:"401k", balance:0, contrib:0, match:0},
+    {type:"roth", balance:0, contrib:0},
+    {type:"brokerage", balance:0},
+    {type:"hsa", balance:0}
+  ],
+
   dependents: [],
+
   assets: {
     cash: 0,
     invest: 0,
     debt: 0
   },
+
   filing: "single"
 };
